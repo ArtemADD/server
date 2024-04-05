3f47a35b2d0c57afdd533a71124099be4f2e7205
ArtemAdd <artm.baranov.08@gmail.com>
2024-04-05 18:23:32 +0500

    s2

--- server.py
+++ server.py
@@ -1,174 +1,131 @@
-from flask import Flask, render_template, request, redirect, abort, make_response, jsonify
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from data.users import User
-from data.jobs import Jobs
-from forms.user import RegisterForm, LoginForm
-from forms.jobs import JobsForm
-from data import db_session, jobs_api
-import os
+# импортируем библиотеки
+from flask import Flask, request, jsonify
+import logging
 
-
+# создаём приложение
+# мы передаём __name__, в нём содержится информация,
+# в каком модуле мы находимся.
+# В данном случае там содержится '__main__',
+# так как мы обращаемся к переменной из запущенного модуля.
+# если бы такое обращение, например, произошло внутри модуля logging,
+# то мы бы получили 'logging'
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-login_manager = LoginManager()
-login_manager.init_app(app)
+
+# Устанавливаем уровень логирования
+logging.basicConfig(level=logging.INFO)
+
+# Создадим словарь, чтобы для каждой сессии общения
+# с навыком хранились подсказки, которые видел пользователь.
+# Это поможет нам немного разнообразить подсказки ответов
+# (buttons в JSON ответа).
+# Когда новый пользователь напишет нашему навыку,
+# то мы сохраним в этот словарь запись формата
+# sessionStorage[user_id] = {'suggests': ["Не хочу.", "Не буду.", "Отстань!" ]}
+# Такая запись говорит, что мы показали пользователю эти три подсказки.
+# Когда он откажется купить слона,
+# то мы уберем одну подсказку. Как будто что-то меняется :)
+sessionStorage = {}
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Registration',
-                                   form=form,
-                                   message="Passwords don't match")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Registration',
-                                   form=form,
-                                   message="There is already such a user")
-        user = User(
-            email=form.email.data,
-            surname=form.surname.data,
-            name=form.name.data,
-            age=form.age.data,
-            position = form.position.data,
-            speciality = form.speciality.data,
-            address = form.address.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Registration', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Incorrect username or password",
-                               form=form)
-    return render_template('login.html', title='Authorization', form=form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
-
-
-@app.route("/")
-def index():
-    db_sess = db_session.create_session()
-    if current_user.is_authenticated:
-        jobs = db_sess.query(Jobs).all()
-        tm = list(map(lambda x: x.id, jobs))
-        users = db_sess.query(User.id, User.surname, User.name).where(User.id.in_(tm)).all()
-        tms = {v[0]: f'{v[1]} {v[2]}' for v in users}
-    else:
-        jobs = db_sess.query(Jobs).all()
-        tm = list(map(lambda x: x.id, jobs))
-        users = db_sess.query(User.id, User.surname, User.name).where(User.id.in_(tm)).all()
-        tms = {v[0]: f'{v[1]} {v[2]}' for v in users}
-    return render_template("index.html", jobs=jobs, tm=tms)
-
-
-@app.route('/jobs',  methods=['GET', 'POST'])
-@login_required
-def add_news():
-    form = JobsForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        jobs = Jobs()
-        jobs.job = form.job.data
-        jobs.team_leader = form.team_leader.data
-        jobs.work_size = form.work_size.data
-        jobs.collaborators = form.collaborators.data
-        jobs.is_finished = form.is_finished.data
-        db_sess.add(jobs)
-        db_sess.commit()
-        return redirect('/')
-    return render_template('jobs.html', title='Adding a job',
-                           form=form, label='Adding a job')
-
-
-@app.route('/jobs/<int:id>', methods=['GET', 'POST'])
-@login_required
-def edit_news(id):
-    form = JobsForm()
-    if request.method == "GET":
-        db_sess = db_session.create_session()
-        jobs = db_sess.query(Jobs).filter(Jobs.id == id).first()
-        if jobs:
-            form.job.data = jobs.job
-            form.team_leader.data = jobs.team_leader
-            form.work_size.data = jobs.work_size
-            form.collaborators.data = jobs.collaborators
-            form.is_finished.data = jobs.is_finished
-        else:
-            abort(404)
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        jobs = db_sess.query(Jobs).filter(Jobs.id == id).first()
-        if jobs:
-            jobs.job = form.job.data
-            jobs.team_leader = form.team_leader.data
-            jobs.work_size = form.work_size.data
-            jobs.collaborators = form.collaborators.data
-            jobs.is_finished = form.is_finished.data
-            db_sess.commit()
-            return redirect('/')
-        else:
-            abort(404)
-    return render_template('jobs.html',
-                           title='Editing a job',
-                           form=form, label='Editing a job'
-                           )
-
-
-@app.route('/jobs_delete/<int:id>', methods=['GET', 'POST'])
-@login_required
-def news_delete(id):
-    db_sess = db_session.create_session()
-    jobs = db_sess.query(Jobs).filter(Jobs.id == id).first()
-    if jobs:
-        db_sess.delete(jobs)
-        db_sess.commit()
-    else:
-        abort(404)
-    return redirect('/')
-
-
+@app.route('/post', methods=['POST'])
+# Функция получает тело запроса и возвращает ответ.
+# Внутри функции доступен request.json - это JSON,
+# который отправила нам Алиса в запросе POST
 def main():
-    db_session.global_init('db/mars_explorer.db')
-    app.register_blueprint(jobs_api.blueprint)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    logging.info(f'Request: {request.json!r}')
+
+    # Начинаем формировать ответ, согласно документации
+    # мы собираем словарь, который потом отдадим Алисе
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    # Отправляем request.json и response в функцию handle_dialog.
+    # Она сформирует оставшиеся поля JSON, которые отвечают
+    # непосредственно за ведение диалога
+    handle_dialog(request.json, response)
+
+    logging.info(f'Response:  {response!r}')
+
+    # Преобразовываем в JSON и возвращаем
+    return jsonify(response)
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и поприветствуем его.
+        # Запишем подсказки, которые мы ему покажем в первый раз
+
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+        # Заполняем текст ответа
+        res['response']['text'] = 'Привет! Купи слона!'
+        # Получим подсказки
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+
+    # Сюда дойдем только, если пользователь не новый,
+    # и разговор с Алисой уже был начат
+    # Обрабатываем ответ пользователя.
+    # В req['request']['original_utterance'] лежит весь текст,
+    # что нам прислал пользователь
+    # Если он написал 'ладно', 'куплю', 'покупаю', 'хорошо',
+    # то мы считаем, что пользователь согласился.
+    # Подумайте, всё ли в этом фрагменте написано "красиво"?
+    if req['request']['original_utterance'].lower() in [
+        'ладно',
+        'куплю',
+        'покупаю',
+        'хорошо'
+    ]:
+        # Пользователь согласился, прощаемся.
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        res['response']['end_session'] = True
+        return
+
+    # Если нет, то убеждаем его купить слона!
+    res['response']['text'] = \
+        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
+    res['response']['buttons'] = get_suggests(user_id)
 
 
-@app.errorhandler(400)
-def bad_request(_):
-    return make_response(jsonify({'error': 'Bad Request'}), 400)
+# Функция возвращает две подсказки для ответа.
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+
+    # Выбираем две первые подсказки из массива.
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+
+    # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+
+    # Если осталась только одна подсказка, предлагаем подсказку
+    # со ссылкой на Яндекс.Маркет.
+    if len(suggests) < 2:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+
+    return suggests
 
 
 if __name__ == '__main__':
-    main()
+    app.run()
